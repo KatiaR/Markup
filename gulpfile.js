8fee193c254286b0476d30a6c67bce95533527e4
Екатерина Романюк <katia-l@mail.ru>
2018-11-02 09:20:06 +0300

    feat: add package.json and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var browserSync = require('browser-sync');
+
+gulp.task('scss', function () {
+	return gulp.src("app/sass/**/*.scss")
+		.pipe(plumber({
+			errorHandler: function (err) {
+				notify.onError({
+					title: "Gulp error in " + err.plugin,
+					message: err.toString()
+				})(err);
+			}
+		}))
+		.pipe(sass())
+		.on('error', function (err) {
+			console.log(err.toString());
+
+			this.emit('end');
+		})
+		.pipe(gulp.dest("app/css"))
+		.pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('browser-sync', function () {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+
+gulp.task('watch', ['browser-sync', 'scss'], function () {
+	gulp.watch("app/sass/**/*.scss", ['scss']);
+	gulp.watch("app/*.html", browserSync.reload);
+	gulp.watch("app/js/**/*.js", browserSync.reload);
+});
+
+
+
